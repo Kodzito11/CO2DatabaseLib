2fedf1e048d9d52059afc3e2624e3dc1256751e3
Kodzito11 <Kos001@edu.zealand.dk>
2024-12-03 12:14:33 +0100

    indsat Repositories

---
+++ CO2DatabaseLib/Repositories/SensorRepository.cs
@@ -0,0 +1,13 @@
+﻿using CO2StatisticRestApi.Models;
+
+namespace CO2StatisticRestApi.Services
+{
+    public class SensorRepository : DBConnection
+    {
+        public Sensor? GetById(int id)
+        {
+            return _dbContext.Sensors.FirstOrDefault(s => s.Id == id);
+        }
+
+    }
+}

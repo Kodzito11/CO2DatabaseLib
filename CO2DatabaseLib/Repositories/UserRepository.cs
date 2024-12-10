6dca54bea81c686a4a54b4f1652b62c544d8617e
Jeppe Jeppsson <jeppe.jeppsson@live.dk>
2024-12-10 10:51:46 +0100

    creating a user requres a "@" and a "."

--- CO2DatabaseLib/Repositories/UserRepository.cs
+++ CO2DatabaseLib/Repositories/UserRepository.cs
@@ -17,10 +17,12 @@ namespace CO2DatabaseLib
 
         public User? Login(string email, string password)
         {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(email))
+                return null;
             // email too long
             if (email.Length > 32)
                 return null;
-            User? user = _dbContext.Users.FirstOrDefault(user => user.Email == email);
+            User? user = _dbContext.Users.FirstOrDefault(u => u.Email == email);
             if (user == null)
                 return null;
             if (!VerifyPassword(password, user))
@@ -30,10 +32,15 @@ namespace CO2DatabaseLib
         }
         public User? Create(string email, string password)
         {
-            // email too long
-            if (email.Length > 32)
-                return null;
             if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+                return null;
+            if (_dbContext.Users.FirstOrDefault(u => u.Email == email) != null)
+                return null;
+            // email too long
+            if (email.Length > 32 ||email.Length < 8)
+                return null;
+            // @ and .
+            if (!email.Contains("@") || !email.Contains("."))
                 return null;
             var user = new User() { Email = email, UserPassword = HashPassword(password) };
             _dbContext.Users.Add(user);
